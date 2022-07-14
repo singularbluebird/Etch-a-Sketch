e345e7941e2cfbbb3cd4e9cfb427ca28574034c2
Samuel <samysarria.ss@gmail.com>
2022-07-14 16:28:55 -0300

    Adds functions and event listeners for the button
    Creates a function that resizes the grid, and then gives the
    button an event listener so that when clicked it'll call said
    function

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let size = 16 * 16;
-let minSize = (1/16) * 100;
+let size;
+let minSize;
+changeGrid(16);
 let divList = [];
 const gridHolder = document.querySelector(".gridHolder");
+const gridResolution = document.querySelector(".gridResolution");
 
 for (i = 0; i < size; i++) {
    divList[i] = document.createElement("div");
@@ -16,5 +18,16 @@ grids.forEach((grid) => {
      grid.style.cssText = `min-width :${minSize}%;`;
      grid.addEventListener("mouseover", () => {
       grid.classList.add("hovered");
-     })
+     });
 });
+
+function pickResolution() {
+   let choice = prompt("How many squares would you like in your grid?");
+   choice = parseInt(choice);
+   return choice;
+}
+
+function changeGrid(num) {
+   size = num**2;
+   minSize = (1/num) * 100;
+}
